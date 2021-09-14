4caef29051b13508dfd1db509b0140f487d58931
Ahsan <Ahsan@BKBHOICT57>
2021-09-14 16:16:44 +0600

    first

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,10 +27,27 @@ namespace TableCheck.Controllers
                 var relTables = db.Database.SqlQuery<TableResult>("[dbo].[getTables]").ToList();
 
                 var searchTables = relTables.Where(o => sourceTables.Contains(o.Table)).ToList();
-
-                foreach(var item in relTables)
+                bool isChecked = false;
+                foreach (var item in sourceTables)
                 {
-                    
+                    List<string> all = UnionOtherListExceptCurrent(searchTables.Where(o => !o.Table.Equals(item)));
+
+                    List<string> currentDependends = searchTables.Where(o => o.Table.Equals(item)).Select(o=>o.DependsON).ToString().Split(',').ToList();
+                    if (all.Contains(item))
+                    {
+                        isChecked = true;
+                        continue;
+                    }
+                    else if (all.Intersect(currentDependends).Any())
+                    {
+                        isChecked = true;
+                        continue;
+                    }else
+                    {
+                        isChecked = false;
+
+                        break;
+                    }
                 }
                 return View();
             }
@@ -39,7 +56,17 @@ namespace TableCheck.Controllers
                 return View();
             }
 
-            
+
+        }
+
+        private List<string> UnionOtherListExceptCurrent(IEnumerable<TableResult> enumerable)
+        {
+            List<string> allTables = new List<string>();
+            foreach (var item in enumerable)
+            {
+                allTables.Union(item.DependsON.Split(',').ToList());
+            }
+            return allTables;
         }
     }
 }
