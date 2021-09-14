1281de1bcb6be1d23f10391c66db63190e91afa7
Ahsan <Ahsan@BKBHOICT57>
2021-09-14 17:18:09 +0600

    seef

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -19,52 +19,64 @@ namespace TableCheck.Controllers
         // GET: Home
         public ActionResult Index(string tables)
         {
-            if (tables != null)
+            if (tables == null)
             {
-                List<string> sourceTables = tables.Split(',').ToList();
+                tables = "Table_2,Table_3";
+            }
+            List<string> sourceTables = tables.Split(',').ToList();
 
-                TestUPEntities1 db = new TestUPEntities1();
-                var relTables = db.Database.SqlQuery<TableResult>("[dbo].[getTables]").ToList();
+            TestUPEntities1 db = new TestUPEntities1();
+            var relTables = db.Database.SqlQuery<TableResult>("[dbo].[getTables]").ToList();
 
-                var searchTables = relTables.Where(o => sourceTables.Contains(o.Table)).ToList();
-                bool isChecked = false;
-                foreach (var item in sourceTables)
+            var searchTables = relTables.Where(o => sourceTables.Contains(o.Table)).ToList();
+            bool isChecked = false;
+            foreach (var item in sourceTables)
+            {
+                List<string> all = UnionOtherListExceptCurrent(searchTables.Where(o => !o.Table.Equals(item)).ToList());
+
+                var currentDependends = searchTables.Where(o => o.Table.Equals(item)).ToList();
+                List<string> nodeDepList = new List<string> { item };
+                foreach (var currentDependent in currentDependends)
                 {
-                    List<string> all = UnionOtherListExceptCurrent(searchTables.Where(o => !o.Table.Equals(item)));
-
-                    List<string> currentDependends = searchTables.Where(o => o.Table.Equals(item)).Select(o=>o.DependsON).ToString().Split(',').ToList();
-                    if (all.Contains(item))
-                    {
-                        isChecked = true;
-                        continue;
-                    }
-                    else if (all.Intersect(currentDependends).Any())
-                    {
-                        isChecked = true;
-                        continue;
-                    }else
-                    {
-                        isChecked = false;
-
-                        break;
-                    }
+                    List<string> newTables = currentDependent.DependsON.Split(',').ToList();
+                    nodeDepList.AddRange(newTables);
                 }
-                return View();
+                if (all.Contains(item))
+                {
+                    isChecked = true;
+                    continue;
+                }
+                else if (nodeDepList.Contains(item))
+                {
+                    isChecked = true;
+                    continue;
+                }
+                else if (all.Intersect(nodeDepList).Any())
+                {
+                    isChecked = true;
+                    continue;
+                }
+                else
+                {
+                    isChecked = false;
+                    TempData["retMsg"] = "Table " + item + " is not joinable among the provided table list";
+                    break;
+                }
             }
-            else
-            {
-                return View();
-            }
-
+            TempData["retSuccess"] = "Table are joinable amongest them";
+            return View();
 
         }
 
-        private List<string> UnionOtherListExceptCurrent(IEnumerable<TableResult> enumerable)
+        private List<string> UnionOtherListExceptCurrent(List<TableResult> enumerable)
         {
-            List<string> allTables = new List<string>();
+            List<string> allTables = new List<string> { };
+
             foreach (var item in enumerable)
             {
-                allTables.Union(item.DependsON.Split(',').ToList());
+                List<string> newTables = item.DependsON.Split(',').ToList();
+
+                allTables.AddRange(newTables);
             }
             return allTables;
         }
